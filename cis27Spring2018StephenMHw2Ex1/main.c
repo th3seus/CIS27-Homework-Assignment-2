f7de5ba5c6a8c00a691d5d2ad2cb70f0dd3978ec
Stephen <macbook@Macbooks-MacBook-Pro-3.local>
2018-02-13 21:41:40 -0800

    defined node struct, typedefs, and declared function prototypes

--- cis27Spring2018StephenMHw2Ex1/main.c
+++ cis27Spring2018StephenMHw2Ex1/main.c
@@ -1,14 +1,39 @@
-//
-//  main.c
-//  cis27Spring2018StephenMHw2Ex1
-//
-//  Created by Stephen on 2/13/18.
-//  Copyright © 2018 Stephen. All rights reserved.
-//
-
+/**
+ * Program Name: CIS27Spring2018StephenMHw2Ex1.c
+ * Discussion:   Homework Assignment #2
+ * Written By:   Stephen Miller
+ * Due Date:     2018/02/23
+ */
 #include <stdio.h>
+#include <stdlib.h>
+
+// Type Definitions
+struct DigitInfoNodeStephenM{
+    /* node to build a linked list in order to count the
+     occurrence of digits in an array of integers supplied
+     by the user */
+    int digit;
+    int count;
+    int size;
+    int* intPtr;
+    struct DigitInfoNodeStephenM* next;
+};
+
+typedef struct DigitInfoNodeStephenM DINT;
+typedef struct DigitInfoNodeStephenM* DINPtrT;
+typedef struct DigitInfoNodeStephenM* DINAddrT;
+
+// function prototypes
+DINPtrT extractDigitInfoStephenM(int*, int);
+DINPtrT createNewNode(int, int, int, int*, DINPtrT);
+void prependNode(DINAddrT, DINPtrT*);
+void appendNode(DINAddrT, DINPtrT);
+void runMenuHw2(void);
+void displayClassInfo(void);
+void displayMenuOptions(void);
+
 
 int main(int argc, const char * argv[]) {
-
+    
     return 0;
 }
