06b5f4eb35e8c56f8bcabf803c122ab345760d41
Stephen <macbook@Macbooks-MacBook-Pro-3.local>
2018-02-14 23:26:36 -0800

    Began work on primary program logic. Significant errors with pointers that need to worked out.

--- cis27Spring2018StephenMHw2Ex1/main.c
+++ cis27Spring2018StephenMHw2Ex1/main.c
@@ -15,7 +15,7 @@ struct DigitInfoNodeStephenM{
     int digit;
     int count;
     int size;
-    int* intPtr;
+    int* iPtr;
     struct DigitInfoNodeStephenM* next;
 };
 
@@ -24,16 +24,108 @@ typedef struct DigitInfoNodeStephenM* DINPtrT;
 typedef struct DigitInfoNodeStephenM* DINAddrT;
 
 // function prototypes
-DINPtrT extractDigitInfoStephenM(int*, int);
-DINPtrT createNewNode(int, int, int, int*, DINPtrT);
-void prependNode(DINAddrT, DINPtrT*);
-void appendNode(DINAddrT, DINPtrT);
+//DINPtrT extractDigitInfoStephenM(int*, int);
+void extractDigitInfoStephenM(int, DINPtrT*); // for testing logic
+//DINPtrT createNewNode(int, int, int, int*, DINPtrT);
+DINPtrT createNewNode(int, int, int, int*);
+void prependNode(DINAddrT myList, DINPtrT*); // adds node to the beginning of list
+void appendNode(DINAddrT, DINPtrT*); // adds node to end of list
+void printList(DINPtrT list);
 void runMenuHw2(void);
 void displayClassInfo(void);
 void displayMenuOptions(void);
 
 
+
 int main(int argc, const char * argv[]) {
+    DINPtrT head = NULL;
+    int testInt = 154288859;
+    int* iPtr = (int*)malloc(sizeof(int));
     
+    *iPtr = 1;
+    
+    head = createNewNode(1, 1, 1, iPtr);
+    
+    extractDigitInfoStephenM(testInt, &head);
+    
+    printList(head);
+    
+    
+    
+    free(iPtr);
+    iPtr = NULL;
     return 0;
+}
+
+void extractDigitInfoStephenM(int num, DINPtrT* list) {
+    int countAry[10] = {0};
+    int* testPtr = &num;
+    int temp = 0;
+    int count = 0;
+    int size = 1;
+    int i = 0;
+    
+    temp = (num < 0) ? -num : num;
+    
+    do {
+        countAry[temp % 10]++;
+        
+        temp /= 10;
+    } while(temp != 0);
+    
+    for (i = 0; i < 10; i++) {
+        if (countAry[i] > 0)
+            count++;
+    }
+    
+    for (i = 0; i < count; i++) {
+        if(countAry[i] > 0)
+            appendNode(createNewNode(i, countAry[i], size, testPtr), list);
+    }
+}
+
+// functions for working with nodes
+
+DINPtrT createNewNode(int digit, int count, int size, int* iPtr) {
+    DINPtrT tempNodePtr = (DINPtrT)malloc(sizeof(DINT));
+    
+    tempNodePtr->digit = digit;
+    tempNodePtr->count = count;
+    tempNodePtr->size = size;
+    tempNodePtr->iPtr = iPtr;
+    
+    return tempNodePtr;
+    
+}
+
+void appendNode(DINAddrT nodeAddr, DINPtrT* listAddr) {
+    DINAddrT tempPtr = NULL;
+    
+    if (*listAddr != NULL) {
+        tempPtr = *listAddr;
+        while (tempPtr->next) { // checks if current node in list is last node
+            tempPtr = tempPtr->next;
+        }
+    } else {
+        *listAddr = nodeAddr;
+        return;
+    }
+    
+    tempPtr->next = nodeAddr; // once while-statement is execute,
+                            // tempPtr->next will be equal to NULL,
+                            // and nodeAddr will be appended to list
+}
+
+void printList(DINPtrT list) {
+    int* tempPtr = NULL;
+    
+    while (list != NULL) {
+        printf("\n digit %d, count %d, size %d", list->digit, list->count, list->size);
+        tempPtr = list->iPtr;
+        for(int i = 0; i < list->size; i++) {
+            printf("\n   integers are %d", *(tempPtr + i));
+        }
+
+        list = list->next;
+    }
 }
