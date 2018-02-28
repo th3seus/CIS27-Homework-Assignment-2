c726c291a2d9d9ad6b1f95f239ef98aea7e31a6a
Stephen <macbook@Macbooks-MacBook-Pro-3.local>
2018-02-27 17:20:56 -0800

    Finished program

--- cis27Spring2018StephenMHw2Ex1/main.c
+++ cis27Spring2018StephenMHw2Ex1/main.c
@@ -2,7 +2,7 @@
  * Program Name: CIS27Spring2018StephenMHw2Ex1.c
  * Discussion:   Homework Assignment #2
  * Written By:   Stephen Miller
- * Due Date:     2018/02/23
+ * Due Date:     2018/03/01
  */
 #include <stdio.h>
 #include <stdlib.h>
@@ -24,16 +24,18 @@ typedef struct DigitInfoNodeStephenM* DINPtrT;
 typedef struct DigitInfoNodeStephenM* DINAddrT;
 
 // function prototypes
-DINAddrT extractDigitInfoStephenM(int*, int/*, DINPtrT* */); // for testing logic
-DINPtrT createNewNode(int, int, int, int*);
-void appendNode(DINPtrT, DINAddrT*); // adds node to end of list
-void printList(DINPtrT);
-DINPtrT buildList(int*);
-DINPtrT populateList(int, int*, DINPtrT);
-void freeList(DINPtrT);
+DINAddrT extractDigitInfoStephenM(int*, int);
+int* countDigits(int*, int);
 void sortArray(int*, int);
 int removeDuplicates(int*, int);
-int* countDigits(int*, int);
+DINPtrT buildList(int*);
+DINPtrT populateList(int, int*, DINPtrT);
+void printList(DINPtrT);
+
+DINPtrT createNewNode(int, int, int, int*);
+void appendNode(DINPtrT, DINAddrT*); // adds node to end of list
+void freeList(DINPtrT*);
+
 void runMenuHw2(void);
 void displayClassInfo(void);
 void displayMenuOptions(void);
@@ -41,44 +43,20 @@ void displayMenuOptions(void);
 
 
 int main(int argc, const char * argv[]) {
-    DINPtrT list = NULL; 
-    int* array = NULL;
-    int origSize = 10;
-
     
-    array = (int*)malloc(origSize * sizeof(int));
+    displayClassInfo();
+    runMenuHw2();
     
-    *(array + 0) = 154487;
-    *(array + 1) = 56369;
-    *(array + 2) = 154487;
-    *(array + 3) = 20;
-    *(array + 4) = 20;
-    *(array + 5) = 37854;
-    *(array + 6) = 46565;
-    *(array + 7) = 37854;
-    *(array + 8) = 154487;
-    *(array + 9) = 7;
-    
-    
-    list = extractDigitInfoStephenM(array, 10);
-    
-    printf("\n outside function\n");
-    printList(list);
-    
-    freeList(list);
-    free(array);
-    array = NULL;
     return 0;
 }
 
-DINAddrT extractDigitInfoStephenM(int* ary, int size/*, DINPtrT* list */) {
+DINAddrT extractDigitInfoStephenM(int* ary, int size) {
     int* countAry = NULL;
     int* tempPtr = NULL;
     DINPtrT head = NULL;
     int noDupSize = 0;
     int i = 0;
 
-    
     // safety checks
     if (countAry != NULL)
         free(countAry);
@@ -95,19 +73,14 @@ DINAddrT extractDigitInfoStephenM(int* ary, int size/*, DINPtrT* list */) {
     
     noDupSize = removeDuplicates(tempPtr, size); // sort and remove duplicates from array
     
-    // for testing purposes
-    for (i = 0; i < 10; i++) {
-        printf("\n %d \n", *(countAry + i));
-    }
-    
     head = buildList(countAry);
-    
-    printList(head);
     
     head = populateList(noDupSize, tempPtr, head);
     
-    printf("\n inside function\n");
-    printList(head);
+    free(countAry);
+    countAry = NULL;
+    free(tempPtr);
+    tempPtr = NULL;
     
     return head;
 }
@@ -120,13 +93,14 @@ DINPtrT populateList(int size, int* array, DINPtrT head) {
     int temp = 0;
     int i = 0;
     int j = 0;
-    
+
     list = head;
     
     while(list) {
+        /* first section counts number of unique integers containing digit correlating to
+            list->digit */
         nodePos = (list)->digit;
         nodePtrSize = 0;
-        
         
         for (i = 0; i < size; i++) {
             temp = (*(array + i) < 0) ? -*(array + i) : *(array + i);
@@ -139,10 +113,13 @@ DINPtrT populateList(int size, int* array, DINPtrT head) {
                 }
             } while (temp != 0);
         }
+        
+        /* second section assigns number of unique digits to list->size, allocates a pointer
+            to an array of integers using nodePtrSize, and finishes by storing the unique integers
+            in the array of integers and assigning that array to list->iPtr */
         (list)->size = nodePtrSize;
         
         tempNodeIPtr = (int*)malloc(nodePtrSize * sizeof(int));
-        
         
         for (i = 0; i < nodePtrSize; i++) {
             for (j = 0; j < size; j++) {
@@ -160,15 +137,9 @@ DINPtrT populateList(int size, int* array, DINPtrT head) {
         }
         
         (list)->iPtr = tempNodeIPtr;
-        for (i = 0; i < list->size; i++) {
-            printf("\n node#: %d, node ptr value: (%d)", list->digit, *(list->iPtr + i));
-        }
-        free(tempNodeIPtr);
-        tempNodeIPtr = NULL;
         
         list = (list)->next;
     }
-    
     return head;
 }
 
@@ -180,7 +151,6 @@ DINPtrT buildList(int* array) {
     head = (DINPtrT)malloc(sizeof(DINT));
     
 
-    
     // begin building linked list
     // beginning with even digits
     for (i = 0; i < 10; i += 2) {
@@ -263,6 +233,72 @@ int* countDigits(int* ary, int size) {
     return countAry;
 }
 
+void runMenuHw2() {
+    int* userArray = NULL;
+    DINPtrT userList = NULL;
+    int option = 0;
+    int size = 0;
+    int i = 0;
+    
+    do {
+        displayMenuOptions();
+        printf("\nEnter an integer for option + ENTER: ");
+        scanf("%d", &option);
+        
+        switch(option) {
+            case 1: // call function
+                printf("\n  How many integers? ");
+                scanf("%d", &size);
+                
+                userArray = (int*)malloc(size * sizeof(int));
+                
+                for (i = 0; i < size; i++) {
+                    printf("\n   Enter integer #%d: ", i + 1);
+                    scanf("%d", (userArray + i));
+                }
+                
+                printf("\n  The original array: ");
+                for (i = 0; i < size; i++) {
+                    printf("\n   %d", *(userArray + i));
+                }
+                
+                printf("\n\nCalling extractDigitInfoStephenM()\n");
+                userList = extractDigitInfoStephenM(userArray, size);
+                
+                printf("\nAfter the function completed and returned the info,"
+                       "\n interesting facts are shown below.");
+                printList(userList);
+                
+                break;
+            case 2: // quit
+                printf("\nQUITTING CAUSE FUN\n");
+                break;
+            default: // wrong option
+                printf("\nWRONG OPTION");
+                break;
+        }
+    } while (option != 2);
+}
+
+void displayMenuOptions() {
+    printf("\n******************************************"
+          "\n*              MENU – HW #2              *"
+          "\n* 1. Calling extractDigitInfoStephenM()  *"
+          "\n* 2. Quit                                *"
+          "\n******************************************");
+}
+
+void displayClassInfo() {
+    printf("CIS 27 - Data Structures\n"
+           "Laney College\n"
+           "Stephen Miller\n\n"
+           "Assignment Information --\n"
+           "  Assignment Number:    Homework 2,\n"
+           "                        Coding Assignment -- Exercise #1\n"
+           "  Written By:           Stephen Miller\n"
+           "  Submitted Date:       2018/03/01\n\n");
+}
+
 // functions for working with nodes
 
 DINPtrT createNewNode(int digit, int count, int size, int* iPtr) {
@@ -275,7 +311,6 @@ DINPtrT createNewNode(int digit, int count, int size, int* iPtr) {
     tempNodePtr->next = NULL;
     
     return tempNodePtr;
-    
 }
 
 void appendNode(DINPtrT nodeAddr, DINAddrT* listAddr) {
@@ -291,9 +326,7 @@ void appendNode(DINPtrT nodeAddr, DINAddrT* listAddr) {
         return;
     }
     
-    tempPtr->next = nodeAddr; // once while-statement is execute,
-                            // tempPtr->next will be equal to NULL,
-                            // and nodeAddr will be appended to list
+    tempPtr->next = nodeAddr;
 }
 
 void printList(DINPtrT head) {
@@ -303,28 +336,30 @@ void printList(DINPtrT head) {
     list = head;
     
     while (list) {
-        printf("\n digit %d, count %d, size %d", list->digit, list->count, list->size);
+        printf("\n   Digit %d is seen %d times; and"
+               "\n      %d can be found in",
+               list->digit, list->count, list->digit);
         tempPtr = list->iPtr;
         for(int i = 0; i < list->size; i++) {
-            printf("\n   integers are %d", *(tempPtr + i));
+            printf("\n        %d", *(tempPtr + i));
         }
         
         list = list->next;
     }
 }
 
-void freeList(DINPtrT list) {
+void freeList(DINAddrT* headAddr) {
     DINPtrT temp = NULL;
+    DINPtrT next = NULL;
+
+    temp = *headAddr; // de-ref the head node to get to the actual node
     
-    if (list == NULL) {
-        return;
-    } else {
-        while (list != NULL) {
-            temp = list;
-            list = list->next;
-            //free(temp->iPtr);
-            free(temp);
-            temp = NULL;
-        }
+    while (temp != NULL) {
+        next = temp->next;
+        free(temp->iPtr); // free iPtr in node
+        free(temp);
+        temp = next;
     }
+    
+    *headAddr = NULL; // set head caller to NULL to kill the real list
 }
